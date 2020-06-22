aa1ad4dfdb2f024b8051a29134de3ca1c89ee24b
AlexReid95 <alexreid95@gmail.com>
2020-06-22 15:42:18 +0100

    Add login component
    Co-authored-by: joepock123 <joe.m.jackson@live.co.uk>

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,10 +1,53 @@
-import React from "react";
-import app from "../base";
+import React, { useCallback, useContext } from "react";
+import { withRouter, Redirect } from "react-router";
+import app from "../base.js";
+import { AuthContext } from "../Auth.js";
 
-export default function Login () {
+const Login = ({ history }) => {
+	const handleLogin = useCallback(
+		async (event) => {
+			event.preventDefault();
+			const { email, password } = event.target.elements;
 
-    return (
-        <h1>Login</h1>
-    )
+			try {
+				await app
+					.auth()
+					.signInWithEmailAndPassword(email.value, password.value);
+				history.push("/");
+			} catch (error) {
+				alert(error);
+			}
+		},
+		[history]
+	);
 
-} 
+	// Checks if user is logged in and redirects if true
+	const { currentUser } = useContext(AuthContext);
+
+	if (currentUser) {
+		return <Redirect to="/" />;
+	}
+
+	return (
+		<div>
+			<h1>Log in</h1>
+			<form onSubmit={handleLogin}>
+				<label>
+					Email
+					<input name="email" type="email" placeholder="Email" />
+				</label>
+				<label>
+					Password
+					<input
+						name="password"
+						type="password"
+						placeholder="Password"
+					/>
+				</label>
+				<button type="submit">Log in</button>
+			</form>
+		</div>
+	);
+};
+
+export default withRouter(Login);
